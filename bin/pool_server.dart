bdeb89a8d841970374466f4b242d202e27cac242
Felipe França <felipefranca824@gmail.com>
2022-11-03 15:32:22 -0300

    add shelf in project

--- bin/pool_server.dart
+++ bin/pool_server.dart
@@ -1,3 +1,14 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+
+void main() async {
+  var handler =
+      const Pipeline().addMiddleware(logRequests()).addHandler(_echoRequest);
+
+  var server = await serve(handler, 'localhost', 8080);
+
+  print('Serving at http://${server.address.host}:${server.port}');
 }
+
+Response _echoRequest(Request request) =>
+    Response.ok('Request for "${request.url}"');
